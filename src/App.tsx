747e766edc0ad564f71082d91f002345c404a064
Jerryzs <jerrysizhang@gmail.com>
2022-11-30 17:30:37 -0800

    Remove unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Quest from "./Components/Quest";
 import Item from "./interfaces/Item";
 import Information from "./Components/Information";
